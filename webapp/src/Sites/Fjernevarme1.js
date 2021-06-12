6d56648652577ab964ab77e1d01b7e783d13987f
Sarah96 <56253152+sara362x@users.noreply.github.com>
2021-06-12 17:05:18 +0200

    kursus side

--- webapp/src/Sites/Fjernevarme1.js
+++ webapp/src/Sites/Fjernevarme1.js
@@ -16,8 +16,39 @@ function Fjernvarme1() {
   return (
     <div className='Fjernvarme1_Grid '>
 <div class="Fjernvarme1_scroll">
-<h3>Fjernvarme AVC - del 1</h3>
-<p></p>
+  <h3 class="white">Fjernvarme AVC - del 1</h3>
+  <div class="box">
+    <div class="column">
+      <h3>Beskrivelse</h3>
+      <br></br>
+      <p>Kurset giver deltagerne en grundig indsigt i fjernvarmens opbygning, installationer og funktioner. Deltagerne kan efter kurset navigere i direkte og indirekte anlæg. Ligesom de har fået forståelse for vigtiheden af god afkøling og kan således rådgive herom.</p>
+      <br></br>
+      <p>Endvidere lærer deltagerne om håndtering af enheder som varmeveksler, varmtvandsbeholdelse og fejlfinding herpå. Kurset giver også en introduktion til Danfoss radiatorventiler og ECL varmestyring samt indregulering og indstilling af Grundfos pumper.</p>
+    </div>
+    <div class="column1">
+      <h3>Praktisk information</h3>
+      <br></br>
+      <p class="bold">Målgruppe</p>
+      <p>VVS-montører</p>
+      <p>VVS-lærlinge</p>
+      <br></br>
+      <p class="bold">Niveau</p>
+      <p>Grundlæggende</p>
+      <br></br>
+      <p class="bold">Varighed</p>
+      <p>7 timer inkl. frokost</p>
+      <br></br>
+      <p class="bold">Tid og sted</p>
+      <p>Nærmere info følger</p>
+      <br></br>
+      <p class="bold">Kursusform</p>
+      <p>Der er både teoretiske og praktiske opgaver</p>
+      <br></br>
+      <p class="bold">Underviser</p>
+      <p>Bob FLD</p>
+      <p>(evt. i samarbejde med konsulent fra Danfoss og Grundfos)</p>
+    </div>
+  </div>
 </div>
     
     </div>
